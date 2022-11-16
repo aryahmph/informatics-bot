11401fb0d470f3476efa3ebf2de05a9f05aeafe8
Arya Yunanta <aryayun90@gmail.com>
2022-11-16 21:23:38 +0700

    add bidang konsentrasi

--- actions/actions.py
+++ actions/actions.py
@@ -3,21 +3,24 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-
 class ActionBidangKonsentrasiDescription(Action):
+    dict = {
+        "sistem cerdas": "ini sistem cerdas ges",
+        "sistem terdistribusi": "kalo ini sistem terdistribusi",
+        "grafis dan visual": "yang ini grafis dan visual"
+    }
+
     def name(self) -> Text:
         return "action_bidang_konsentrasi_description"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         bidang_konsentrasi = tracker.get_slot("bidang_konsentrasi")
-        if not bidang_konsentrasi:
+        if bidang_konsentrasi and bidang_konsentrasi.lower() in self.dict:
             dispatcher.utter_message(
-                template="utter_description_bidang_konsentrasi",
-                text="Maaf, bidang konsentrasi tersebut tidak terdata pada sistem kami"
+                text=f"{self.dict[bidang_konsentrasi]}!"
             )
         else:
             dispatcher.utter_message(
-                template="utter_description_bidang_konsentrasi",
-                text=f"pilihan mu: {bidang_konsentrasi}!"
+                text="Bidang konsentrasi yang anda masukkan tidak valid"
             )
         return []
